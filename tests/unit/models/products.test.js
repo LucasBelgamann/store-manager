99bd51e3b493b47822dbfcfff5f2ec334ae9845e
LucasBelgamann <lucasbelgamann@hotmail.com>
2022-09-11 13:19:26 -0300

    test[models] getAll, getById

---
+++ tests/unit/models/products.test.js
@@ -0,0 +1,51 @@
+const { expect } = require('chai');
+const sinon = require('sinon');
+
+const productsModel = require('../../../src/models/products.model');
+const connection = require('../../../src/models/connection')
+
+describe('Teste da pasta models', () => {
+    beforeEach(sinon.restore);
+  it('Testando a função getProducts', async () => {
+    // const execute = [{
+    //       id: 1,
+    //       name: 'Martelo de Thor',
+    //     },
+    //     {
+    //       id: 2,
+    //       name: 'Traje de encolhimento',
+    //   },
+    //     {
+    //       id: 3,
+    //       name: 'Escudo do Capitão América',
+    //     },
+    //   ];
+    // sinon.stub(connection, 'execute').resolves([execute]);
+    const response = await productsModel.getProducts();
+    expect(response).to.be.an('array');
+  });
+
+  describe('testando a função getById', function () {
+    before(function () {
+      const execute = [{
+          id: 1,
+          name: 'Martelo de Thor',
+        },
+      ];
+
+      sinon.stub(productsModel, 'getById').resolves(execute);
+    })
+    it('com sucesso', async function () {
+      const expected = [
+        {
+          id: 1,
+          name: 'Martelo de Thor',
+        },
+      ];
+
+      const response = await productsModel.getById(1);
+
+      expect(response).to.deep.equal(expected);
+    });
+  });
+})
